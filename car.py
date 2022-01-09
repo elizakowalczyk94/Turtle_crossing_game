55e4bec258c22d0469d41336adb5e13f2109e73e
Eliza Kowalczyk <elizakowalczyk94@gmail.com>
2022-01-09 20:21:22 +0100

    Add car class functions

--- car.py
+++ car.py
@@ -1,24 +1,38 @@
+import time
 from turtle import Turtle
 import random
 
 COLORS = ["yellow", "gold", "orange", "red", "maroon", "violet", "magenta", "purple", "navy", "blue", "skyblue", "cyan",
           "turquoise", "lightgreen", "green", "darkgreen", "chocolate", "brown", "gray", "white"]
 
-MOVE_DISTANCE = 20
+MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
 
 
 class Car(Turtle):
 
-    def __init__(self):
+    def __init__(self, car_no=10):
         super().__init__()
-        self.penup()
-        self.shape("square")
-        self.color(random.choice(COLORS))
-        self.shapesize(stretch_wid=1, stretch_len=2)
-        self.x_start = 300
-        self.y_start = random.choice(range(-270, 270))
-        self.goto(self.x_start, self.y_start)
+        self.car_no = car_no
+        self.cars_list = []
+        for _ in range(self.car_no):
+            self.create_cars()
 
-    def move(self):
-        new_x = self.xcor() - MOVE_DISTANCE
-        self.goto(new_x, self.y_start)
+    def create_cars(self):
+        new_car = Turtle(shape="square")
+        new_car.penup()
+        new_car.setheading(180)
+        new_car.color(random.choice(COLORS))
+        new_car.shapesize(stretch_wid=1, stretch_len=2)
+        new_car.x_start = random.choice(range(300, 900))
+        new_car.y_start = random.choice(range(-270, 270))
+        new_car.goto(new_car.x_start, new_car.y_start)
+        self.cars_list.append(new_car)
+
+    def move_all(self):
+        for car in self.cars_list:
+            if car.xcor() < -300:
+                new_y = random.choice(range(-270, 270))
+                car.goto(300, new_y)
+            else:
+                car.forward(MOVE_DISTANCE)
