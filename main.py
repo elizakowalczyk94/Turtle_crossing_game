b4f510b886f77112d5cdbfd9f015d1239ab23949
Eliza Kowalczyk <elizakowalczyk94@gmail.com>
2022-01-09 09:57:08 +0100

    Add car object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 import time
+import car
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -7,11 +8,13 @@ screen.title("Turtle Crossing Game")
 screen.bgcolor("black")
 
 screen.tracer(0)
+car = car.Car()
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(0.3)
     screen.update()
 
+    car.move()
 
 screen.exitonclick()
