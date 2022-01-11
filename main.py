5e2488e85ea350bab9c8cf9e127a364243597c7f
Eliza Kowalczyk <elizakowalczyk94@gmail.com>
2022-01-11 09:20:42 +0100

    Add create_cars and move_all into while loop

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from turtle import Screen
 import time
 import car
-import random
+import user_turtle
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -12,12 +12,22 @@ screen.tracer(0)
 
 car_list = []
 
-cars = car.Car(10)
+cars = car.Car()
+user_turtle_obj = user_turtle.UserTurtle()
+
+screen.listen()
+screen.onkey(user_turtle_obj.move_up, "Up")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    cars.create_car()
     cars.move_all()
+
+    # detect score
+    if user_turtle_obj.ycor() > 280:
+        print("score")
 
 screen.exitonclick()
